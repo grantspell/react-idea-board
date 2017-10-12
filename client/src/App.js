494e19bf5bbd23d5097b1035bc31b523191433c3
grantspell <spellgrant@gmail.com>
2017-10-12 14:29:10 -0400

    React Router set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+import HomePage from './components/home/HomePage'
+import IdeaPage from './components/idea/IdeaPage'
+import LogInPage from './components/login/LogInPage'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+          <div>
+            <div>
+              <div>
+                <Link to="/">Home</Link>
+              </div>
+              <div>
+                <Link to="/login">Login</Link>
+              </div>
+              <div>
+                <Link to="/idea">Idea</Link>
+              </div>
+            </div>
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/idea" component={IdeaPage} />
+              <Route exact path="/login" component={LogInPage} />
+            </Switch>
+          </div>
+      </Router>
+    )
   }
 }
 
---
+++ client/src/components/login/LogInPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LogInPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>WELCOME TO THE LOG IN PAGE</h1>
+            </div>
+        );
+    }
+}
+
+export default LogInPage;
