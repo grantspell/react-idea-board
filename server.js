136375f171be17ee8eb5f60f0fd7a375bbc69740
grantspell <spellgrant@gmail.com>
2017-10-12 10:59:06 -0400

    CREATED BASIC SERVER.JS || CONNECTED TO MONGODB

---
+++ server.js
@@ -0,0 +1,35 @@
+// Imports variables from .env file
+require("dotenv").config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+mongoose.Promise = global.Promise
+
+// Create a new app using express
+const app = express()
+
+// Connect to MongoDB and set up messages for when
+// Mongo connects successfully or errors out
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true})
+const connection = mongoose.connection;
+
+connection.on('connect', () => {
+    console.log('SUCCESSFULLY CONNECTED TO MONGODB')
+})
+
+connection.on('error', (err) => {
+    console.log('MONGODB Error: ', err)
+})
+
+// Inject middleware
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+// Set App to specific port
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(` || PORT: ${PORT} IS HOT || `)
+})
