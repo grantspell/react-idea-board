f31ee2a5758a1a354abaaaf07cd498e5330953df
grantspell <spellgrant@gmail.com>
2017-10-12 11:19:26 -0400

    ADDED CREATE-REACT-APP

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 })
 
 // Set App to specific port
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(` || PORT: ${PORT} IS HOT || `)
 })
